4218df105161c29d046a228e7d5a1b266da8fa41
Ray000000 <94271266+Ray000000@users.noreply.github.com>
2023-08-29 14:39:12 +0800

    Update xray-update-log.sh

--- xray-update-log.sh
+++ xray-update-log.sh
@@ -3,3 +3,4 @@ clear
 echo -e "\e[1m\e[32m〔Beta 0.1.1〕\e[0m: 更新 Update-Log.sh 功能"
 echo -e "\e[1m\e[32m〔Beta 0.1.2〕\e[0m: 修正部分 Shell 錯誤"
 echo -e "\e[1m\e[32m〔Beta 0.1.3〕\e[0m: 更新 Store.sh 的內容"
+echo -e "\e[1m\e[32m〔Beta 0.1.3.1〕\e[0m: 修正 Store.sh 內容的連結錯誤"
