986825bcdbeba312c2d760ef3df799f4069a6afa
Ray000000 <st0105117@gmail.com>
2023-11-14 21:51:41 +0800

    Update xray-zh-hant-store.sh

--- file/zh-hant/xray-zh-hant-store.sh
+++ file/zh-hant/xray-zh-hant-store.sh
@@ -22,7 +22,7 @@ read -p "請輸入：" choice
 
 if [[ $choice == "1" ]]; then
   curl -sS -O https://ray000000.github.io/Shell/file/zh-hant/app/xray-zh-hant-docker.sh && chmod +x xray-zh-hant-docker.sh && sudo ./xray-zh-hant-docker.sh
-if [[ $choice == "2" ]]; then
+elif [[ $choice == "2" ]]; then
   curl -sS -O https://ray000000.github.io/Shell/file/zh-hant/app/xray-zh-hant-nginx-proxy-manager.sh && chmod +x xray-zh-hant-nginx-proxy-manager.sh && sudo ./xray-zh-hant-nginx-proxy-manager.sh
 
 elif [[ $choice == "00" ]]; then
