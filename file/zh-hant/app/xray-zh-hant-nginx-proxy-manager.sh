c592e4110b27ec3fd40a73ed0662ef01da97d26b
Ray000000 <st0105117@gmail.com>
2023-11-14 23:44:02 +0800

    New App! Nginx Proxy Manager!

--- file/zh-hant/app/xray-zh-hant-nginx-proxy-manager.sh
+++ file/zh-hant/app/xray-zh-hant-nginx-proxy-manager.sh
@@ -21,10 +21,7 @@ echo -e "\e[1m\e[93m
 請選擇您要執行的任務：
 \e[0m"
 echo "1. 安裝"
-echo "2. 更新"
-echo "3. 解除安裝"
-echo -e "\e[1m\e[31m4. 解除安裝（不保存資料）\e[0m"
-echo "5. 查看版本"
+echo -e "\e[1m\e[31m2. 解除安裝（不保存資料）\e[0m"
 echo -e "\e[1m\e[32m0. 回到菜單\e[0m"
 
 read -p "請輸入：" choice
@@ -34,28 +31,15 @@ if [[ $choice == "1" ]]; then
   echo -e "\e[1m\e[31mN. 取消安裝\e[0m"
   read -p "請輸入：" yn_choice
 elif [[ $choice == "2" ]]; then
-  sudo apt-get update -y && sudo apt-get upgrade docker-ce -y && sudo apt-get upgrade docker-compose -y
-  read -n 1 -p "按任意按鍵以繼續"
-  sudo ./xray-zh-hant-docker.sh
-elif [[ $choice == "3" ]]; then
   echo -e "\e[1m\e[34mY. 確認解除安裝\e[0m"
   echo -e "\e[1m\e[31mN. 取消解除安裝\e[0m"
   read -p "請輸入：" yn2_choice
-elif [[ $choice == "4" ]]; then
-  echo -e "\e[1m\e[34mY. 確認解除安裝\e[0m"
-  echo -e "\e[1m\e[31mN. 取消解除安裝\e[0m"
-  read -p "請輸入：" yn3_choice
-elif [[ $choice == "5" ]]; then
-  docker --version
-  docker-compose --version
-  read -n 1 -p "按任意按鍵以繼續"
-  sudo ./xray-zh-hant-docker.sh
 elif [[ $choice == "0" ]]; then
   sudo ./xray-zh-hant-store.sh
 else
   echo -e "\e[1m\e[31m錯誤：無效選項\e[0m"
   read -n 1 -p "按任意按鍵，回到菜單"
-  sudo ./xray-zh-hant-docker.sh
+  sudo ./xray-zh-hant-nginx-proxy-manager.sh
 fi
 
 case $yn_choice in
@@ -66,59 +50,74 @@ case $yn_choice in
       echo "尚未安裝 Docker"
       sudo ./xray-zh-hant-docker.sh
     fi
-    if [ -f "/etc/debian_version" ]; then
-      DEBIAN_FRONTEND=noninteractive apt update -y
-      DEBIAN_FRONTEND=noninteractive apt full-upgrade -y
-      DEBIAN_FRONTEND=noninteractive apt upgrade -y
-      DEBIAN_FRONTEND=noninteractive apt autoremove -y
-      DEBIAN_FRONTEND=noninteractive apt autoclean -y
-      DEBIAN_FRONTEND=noninteractive apt-get install -y curl wget sudo nano htop socat neofetch
-    fi
-    if [ -f "/etc/redhat-release" ]; then
-      yum -y update
-      yum -y install curl wget sudo nano htop socat neofetch
-    fi
-    curl -fsSL https://get.docker.com | sh
-    curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
+    commands=(
+        "sudo docker pull jc21/nginx-proxy-manager:latest"
+        "sudo docker run -d -p 80:80 -p 81:81 -p 443:443 --name nginx-proxy-manager jc21/nginx-proxy-manager:latest"
+        "sudo docker update --restart=always nginx-proxy-manager"
+      )
+
+    total_commands=${#commands[@]}
+
+    for ((i = 0; i < total_commands; i++)); do
+      command="${commands[i]}"
+      eval $command
+        percentage=$(( (i + 1) * 100 / total_commands ))
+        completed=$(( percentage / 2 ))
+        remaining=$(( 50 - completed ))
+        progressBar="["
+        for ((j = 0; j < completed; j++)); do
+          progressBar+="#"
+        done
+        for ((j = 0; j < remaining; j++)); do
+          progressBar+="-"
+        done
+        progressBar+="]"
+        echo -ne "\r\e[1m\e[93m\e[1m\e[32m[$percentage%]\e[0m $progressBar\e[0m"
+      done
+
+      echo
 
     read -n 1 -p "按任意按鍵以繼續"
-    sudo ./xray-zh-hant-docker.sh
+    sudo ./xray-zh-hant-nginx-proxy-manager.sh
     ;;
   [Nn])
-    sudo ./xray-zh-hant-docker.sh
+    sudo ./xray-zh-hant-nginx-proxy-manager.sh
     ;;
 esac
   
 case $yn2_choice in
   [Yy])
-    sudo apt-get remove docker -y
-    sudo apt-get remove docker-ce -y
-    sudo apt-get purge docker-ce -y
-    sudo rm -rf /var/lib/docker
-    sudo rm /usr/local/bin/docker-compose
+    commands=(
+        "sudo docker update --restart=no nginx-proxy-manager"
+        "sudo docker stop nginx-proxy-manager:latest"
+        "sudo docker rm nginx-proxy-manager:latest"
+      )
+
+    total_commands=${#commands[@]}
+
+    for ((i = 0; i < total_commands; i++)); do
+      command="${commands[i]}"
+      eval $command
+        percentage=$(( (i + 1) * 100 / total_commands ))
+        completed=$(( percentage / 2 ))
+        remaining=$(( 50 - completed ))
+        progressBar="["
+        for ((j = 0; j < completed; j++)); do
+          progressBar+="#"
+        done
+        for ((j = 0; j < remaining; j++)); do
+          progressBar+="-"
+        done
+        progressBar+="]"
+        echo -ne "\r\e[1m\e[93m\e[1m\e[32m[$percentage%]\e[0m $progressBar\e[0m"
+      done
+
+      echo
 
     read -n 1 -p "按任意按鍵以繼續"
-    sudo ./xray-zh-hant-docker.sh
+    sudo ./xray-zh-hant-nginx-proxy-manager.sh
     ;;
   [Nn])
-    sudo ./xray-zh-hant-docker.sh
-    ;;
-esac
-
-case $yn3_choice in
-  [Yy])
-    docker rm $(docker ps -a -q) && docker rmi $(docker images -q) && docker network prune
-    sudo apt-get remove docker -y
-    sudo apt-get remove docker-ce -y
-    sudo apt-get purge docker-ce -y
-    sudo rm -rf /var/lib/docker
-    sudo rm /usr/local/bin/docker-compose
-
-    read -n 1 -p "按任意按鍵以繼續"
-    sudo ./xray-zh-hant-docker.sh
-    ;;
-  [Nn])
-    sudo ./xray-zh-hant-docker.sh
+    sudo ./xray-zh-hant-nginx-proxy-manager.sh
     ;;
 esac
