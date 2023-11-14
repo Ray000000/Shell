c09f009f8cddb1e2660a8b3aecb76b874f54e6f3
Ray000000 <94271266+Ray000000@users.noreply.github.com>
2023-11-14 13:44:40 +0000

    Update xray-update-log.sh

--- xray-update-log.sh
+++ xray-update-log.sh
@@ -5,4 +5,3 @@ echo -e "\e[1m\e[32m〔Beta 0.2.1〕\e[0m: 內容翻譯（透過 Google Bard）"
 echo -e "\e[1m\e[32m〔Beta 0.2.2〕\e[0m: 對於內容錯誤的修正"
 echo -e "\e[1m\e[32m〔Beta 0.2.3〕\e[0m: 內容翻譯-2"
 echo -e "\e[1m\e[32m〔Beta 0.2.4〕\e[0m: 新增系統重裝"
-;;
