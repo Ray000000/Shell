088ece272f35778199826712c39a19bc8366515f
Ray000000 <st0105117@gmail.com>
2023-11-18 19:03:52 +0800

    Update xray-zh-hant-matrix.sh

--- file/zh-hant/app/xray-zh-hant-matrix.sh
+++ file/zh-hant/app/xray-zh-hant-matrix.sh
@@ -58,8 +58,8 @@ case $yn_choice in
     else
       echo "Docker 已安裝"
     fi
-    read -p "請輸入您的網址(Ex: chat.example.com)：" choice1
       sudo -i
+      read -p "請輸入網址：" choice1
       mkdir -p /root/data/docker/matrix
       cd /root/data/docker/matrix
       sudo docker run -it --rm \
