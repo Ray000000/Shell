c8d29c524ffc5b29fc90b544ee679031cb026f5b
Ray000000 <st0105117@gmail.com>
2023-12-22 22:52:10 +0800

    Update xray-zh-hant-store-a.sh

--- file/zh-hant/xray-zh-hant-store-a.sh
+++ file/zh-hant/xray-zh-hant-store-a.sh
@@ -13,7 +13,7 @@ _/      _/  _/    _/    _/_/_/    _/_/_/           ____|_/ |_|  |_| |_|____ |_|_
 echo -e "\e[1m\e[93m
 請選擇您要安裝應用程式：
 \e[0m"
-echo "1. None"
+echo "1. Alist"
 echo -e "\e[1m\e[32m0. Exit\e[0m"
 
 read -p "請輸入：" choice
