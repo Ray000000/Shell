e9f1af504a6d12bc8c60ba44285efc620bb89980
Ray000000 <st0105117@gmail.com>
2023-12-29 19:30:56 +0800

    t

--- file/zh-hant/xray-zh-hant-store - 複製.sh
+++ file/zh-hant/xray-zh-hant-store - 複製.sh
@@ -24,15 +24,15 @@ echo -e "\e[1m\e[93m
 請選擇您要安裝應用程式：
 \e[0m"
 
-# List all available apps from xray-hant-app.txt
-app_list=$(curl -sS https://raw.githubusercontent.com/Ray000000/Shell/main/file/zh-hant/xray-hant-app.txt)
+app_list_url="https://raw.githubusercontent.com/Ray000000/Shell/main/file/zh-hant/xray-zh-hant-app.txt"
+app_list_raw=$(curl -sS "$app_list_url")
+app_list_processed=$(echo "$app_list_raw" | sed 's/xray-zh-hant-\(.*\)\.sh/\1/')
 
-# Display the app list with numbers
 index=1
 while IFS= read -r app; do
   echo "$index. $app"
   ((index++))
-done <<< "$app_list"
+done <<< "$app_list_processed"
 
 echo -e "\e[1m\e[32m0. Exit\e[0m"
 
@@ -42,10 +42,8 @@ if [[ $choice == "0" ]]; then
   exit
 fi
 
-# Get the selected app name
-selected_app=$(echo "$app_list" | sed -n "${choice}p")
+selected_app=$(echo "$app_list_processed" | sed -n "${choice}p")
 
-# Execute the selected app
-curl -sS -O "https://raw.githubusercontent.com/Ray000000/Shell/main/file/zh-hant/app/xray-zh-hant-${selected_app}" && chmod +x "xray-zh-hant-${selected_app}" && sudo "./xray-zh-hant-${selected_app}"
+curl -sS -O "https://raw.githubusercontent.com/Ray000000/Shell/main/file/zh-hant/app/xray-zh-hant-${selected_app}.sh" && chmod +x "xray-zh-hant-${selected_app}.sh" && sudo "./xray-zh-hant-${selected_app}.sh"
 
 echo "${script_name} ended: $(date)" >> ./xray-log.txt
