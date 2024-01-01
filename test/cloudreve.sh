65b24279bc4c705fa91ef632c9170a8aa55670b0
Ray000000 <st0105117@gmail.com>
2024-01-01 10:19:39 +0800

    Create cloudreve.sh

---
+++ test/cloudreve.sh
@@ -0,0 +1,36 @@
+sudo wget https://github.com/cloudreve/Cloudreve/releases/download/3.8.3/cloudreve_3.8.3_linux_amd64.tar.gz
+tar -zxvf cloudreve_3.8.3_linux_amd64.tar.gz
+chmod +x ./cloudreve
+./cloudreve
+echo "
+[Unit]
+Description=Cloudreve
+Documentation=https://docs.cloudreve.org
+After=network.target
+After=mysqld.service
+Wants=network.target
+
+[Service]
+WorkingDirectory=/root
+ExecStart=/root/cloudreve
+Restart=on-abnormal
+RestartSec=5s
+KillMode=mixed
+
+StandardOutput=null
+StandardError=syslog
+
+[Install]
+WantedBy=multi-user.target" >> /usr/lib/systemd/system/cloudreve.service
+systemctl daemon-reload
+systemctl start cloudreve
+systemctl enable cloudreve
+
+
+systemctl start cloudreve
+
+systemctl stop cloudreve
+
+systemctl restart cloudreve
+
+systemctl status cloudreve
